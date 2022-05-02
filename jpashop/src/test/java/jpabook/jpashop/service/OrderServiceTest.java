6309a481ee9d28f474803f79fb8af591b12156b5
semin-1432 <2007ksm@naver.com>
2022-05-02 21:57:17 +0900

    order domin development

---
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Test
+    public void 상품주문() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+}
