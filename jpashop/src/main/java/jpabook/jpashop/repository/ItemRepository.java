c153d0a43d180297490e550b78eba46f1d540abd
semin-1432 <2007ksm@naver.com>
2022-05-11 21:25:08 +0900

    about Item

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -3,14 +3,18 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
-    private final EntityManager em;
+
+    @PersistenceContext
+    EntityManager em;
 
     public void save(Item item){
         if(item.getId()==null){
